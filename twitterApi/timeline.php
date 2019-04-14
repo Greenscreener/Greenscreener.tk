07a39d7cc2040355f0305a614456e70ff3e2f0ce
Greenscreener <honzikcernoh@gmail.com>
2019-04-14 18:25:22 +0200

    I have no idea what happened here, commit before firebase transfer.

---
+++ twitterApi/timeline.php
@@ -0,0 +1,10 @@
+<?php
+
+include "./secrets.php";
+require "./twitteroauth/autoload.php";
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
+$content = $connection->get("account/verify_credentials");
+$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
