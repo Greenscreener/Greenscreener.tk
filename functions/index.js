c215b2c682edc7bef3d1423672082284eaae30dd
Greenscreener <honzikcernoh@gmail.com>
2019-04-20 22:43:07 +0200

    Added social section and footer.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin:true });
+const fetch = require('node-fetch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.localGuideInfo = functions.https.onRequest((req, response) => {
+    return cors(req, response, () => {
+        return fetch("https://www.google.com/maps/contrib/116242286923394079188/").then(res => res.text()).then(res => {
+            response.send(res);
+        });
+    });
+});
+
